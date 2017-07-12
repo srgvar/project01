f60ebd840140c3ac96e925c3e438930a920f101e
Сергей Варакин <srgvar@narod.ru>
2017-07-12 20:15:15 +0700

    добавил класс MyFirstClass

---
+++ src/main/java/MyFirstClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by srgva on 12.07.2017.
+ */
+public class MyFirstClass {
+    public static void main(String... args){
+        System.out.println("Hello! My first class in Intelij IDEA :)");
+    }
+}
